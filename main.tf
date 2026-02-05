1a5527ed879fa168ad6944bb45ceaed45131db1a
Your Name <vemasani.pavani@gmail.com>
2026-02-05 12:10:28 +0530

    create autoscaling group and attach launch template

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 // create a launch template
-resource "aws_launch_template" "foo" {
+resource "aws_launch_template" "lt" {
   name = "${var.env}-${var.component}"
   image_id =var.ami
   instance_type = "t2.micro"
@@ -13,4 +13,19 @@ resource "aws_launch_template" "foo" {
   user_data = base64encode(templatefile("${path.module}/user-data.sh", {}))
 }
 
+resource "aws_autoscaling_group" "bar" {
+  name                      = "${var.env}-${var.component}"
+  max_size                  = 5
+  min_size                  = 1
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+   tag {
+    key                 = "${var.env}-${var.component}"
+    value               = "${var.env}-${var.component}"
+    propagate_at_launch = true
+  }
 
+
+}
