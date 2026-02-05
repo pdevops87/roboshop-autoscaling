a8de09c5f93d024d79a66f4b7f03ee5f9fe9bda6
Your Name <vemasani.pavani@gmail.com>
2026-02-05 12:12:51 +0530

    add availability zone(data center)

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "aws_autoscaling_group" "bar" {
   name                      = "${var.env}-${var.component}"
   max_size                  = 5
   min_size                  = 1
+  availability_zones = ["us-east-1a","us-east-1b"]
   launch_template {
     id      = aws_launch_template.lt.id
     version = "$Latest"
