5b69438cb652dc97fdb94b82793fb152edb7df18
Your Name <vemasani.pavani@gmail.com>
2026-02-07 14:32:10 +0530

    resource_type

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "aws_launch_template" "lt" {
   image_id =var.ami
   instance_type = "t2.micro"
    tag_specifications {
-    resource_type = aws_launch_template.lt.name
+    resource_type = "instance"
     tags = {
-      Name = "${var.env}-${var.component}"
+      Name = var.component
     }
   }
   user_data = base64encode(templatefile("${path.module}/user-data.sh", {}))
