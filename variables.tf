7512a42d0ac22931653fc2d1bf456f8d28cac88f
Your Name <vemasani.pavani@gmail.com>
2026-02-10 13:01:54 +0530

    create db_components and security group for different db servers

--- variables.tf
+++ variables.tf
@@ -1,26 +1,6 @@
 variable "env"{
   default="dev"
 }
-
-db_components = {
-  mongodb = {
-     ports = { ssh=22 , app= 27017 }
-     instance_type = "t3.micro"
-  }
-  redis = {
-    ports = { ssh=22 , app=6379 }
-    instance_type = "t3.micro"
-  }
-  mysql = {
-    ports = { ssh=22 , app=3306 }
-    instance_type = "t3.micro"
-  }
-  rabbitmq = {
-    ports = { ssh=22 , app=5672 }
-    instance_type = "t3.micro"
-  }
-}
-variable "db_components" {}
 variable "ami" {
   default = "ami-0220d79f3f480ecf5"
 }
@@ -32,4 +12,25 @@ variable "subnet_id" {
 }
 variable "vpc_id" {
   default = "vpc-02a94ee8944923438"
+}
+
+variable "db_components" {
+  default = {
+    mongodb = {
+      ports = { ssh = 22, app = 27017 }
+      instance_type = "t3.micro"
+    }
+    redis = {
+      ports = { ssh = 22, app = 6379 }
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      ports = { ssh = 22, app = 3306 }
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      ports = { ssh = 22, app = 5672 }
+      instance_type = "t3.micro"
+    }
+  }
 }
