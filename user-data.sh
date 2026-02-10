47843a9d864fa23bfd6dd12d6b2a7df4b163d029
Your Name <vemasani.pavani@gmail.com>
2026-02-10 13:37:43 +0530

    vpc security group id

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install python3.11-pip -y,
-pip3.11 install ansible,
+sudo dnf install python3.11-pip -y,
+sudo pip3.11 install ansible,
 ansible-pull -i localhost, -U https://github.com/pdevops87/roboshop-ansible-v4 roboshop.yaml -e component=${component} -e env=${env}
