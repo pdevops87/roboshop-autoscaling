34c0bb460678c407f9f267c82702e160b423aeca
Your Name <vemasani.pavani@gmail.com>
2026-02-10 18:35:52 +0530

    create target group and application load balancer

--- lb.tf
+++ lb.tf
@@ -1,20 +1,19 @@
-# resource "aws_lb" "lb" {
-#   for_each           = var.app_components
-#   name               = "${each.key}-${var.env}-lb"
-#   internal           = false // public
-#   load_balancer_type = "application"
-#   security_groups = [aws_security_group.app_sg[each.key].id]
-#
-#   subnets            = ["subnet-04ce96f612c9d802d","subnet-0e4eadfc446b55f58"]
-#    tags = {
-#     Name = "${var.env}-${var.component}-lb"
-#   }
-# }
-# resource "aws_lb_target_group" "tg" {
-#
-#   name     = "${var.env}-${var.component}-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = var.vpc_id
-#   target_type = "instance"
-# }
+resource "aws_lb" "lb" {
+  for_each           = var.app_components
+  name               = "${each.key}-${var.env}-lb"
+  internal           = false // public
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.app_sg[each.key].id]
+  subnets            = ["subnet-04ce96f612c9d802d","subnet-0e4eadfc446b55f58"]
+   tags = {
+    Name = "${var.env}-${each.key}-lb"
+  }
+}
+resource "aws_lb_target_group" "tg" {
+  for_each           = var.app_components
+  name     = "${var.env}-${each.key}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "instance"
+}
