5706f563c5eb542d9b1a9c5c2f89ee486fdd610e
Your Name <vemasani.pavani@gmail.com>
2026-02-10 13:50:25 +0530

    remove base64encode

--- db_server.tf
+++ db_server.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "db_servers" {
   ami              =  var.ami
   instance_type    = each.value["instance_type"]
   vpc_security_group_ids = [aws_security_group.sg[each.key].id]
-  user_data_base64 = base64encode(templatefile("${path.module}/user-data.sh", {
+  user_data = templatefile("${path.module}/user-data.sh", {
     component      = each.key
     env            = var.env
-  }))
+  })
   tags = {
     Name = "${each.key}-${var.env}"
   }
