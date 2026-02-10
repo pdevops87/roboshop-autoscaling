7258966b74e0a54000711bdb4d9d43714d035500
Your Name <vemasani.pavani@gmail.com>
2026-02-10 13:07:17 +0530

    value chaned

--- security_gp.tf
+++ security_gp.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "sg" {
   name = "${each.key}-${var.env}"
   vpc_id = "vpc-02a94ee8944923438"
   dynamic "ingress" {
-    for_each = each.value["ports"]["app"]
+    for_each = each.value.ports.app
     content {
       from_port = ingress.value
       to_port   = ingress.value
