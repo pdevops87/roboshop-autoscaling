6d18bf2dfe378dad38927ec8789dc30e58671219
Your Name <vemasani.pavani@gmail.com>
2026-02-10 18:49:36 +0530

    create cname records

--- route53.tf
+++ route53.tf
@@ -1,5 +1,9 @@
-# resource "aws_route53_record" "app_records" {
-#   name    = ""
-#   type    = "CNAME"
-#   zone_id = var.zone_id
-# }
+resource "aws_route53_record" "app_records" {
+  depends_on = [aws_lb.lb]
+  for_each = var.app_components
+  name    = "${each.key}-${var.env}"
+  type    = "CNAME"
+  zone_id = var.zone_id
+  records = [aws_lb.lb[each.key].dns_name]
+  ttl = 30
+}
