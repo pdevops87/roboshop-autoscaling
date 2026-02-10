dcfd88cfe36a83851530b5eed48a7e0aea08175c
Your Name <vemasani.pavani@gmail.com>
2026-02-10 19:27:56 +0530

    encoding userdata

--- asg.tf
+++ asg.tf
@@ -4,17 +4,16 @@ resource "aws_launch_template" "lt" {
   image_id =var.ami
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [aws_security_group.app_sg[each.key].id]
-  user_data = templatefile("${path.module}/user-data.sh", {
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     component    = each.key
     env          = var.env
-  })
+  }))
   tag_specifications {
     resource_type = "instance"
     tags = {
       Name = each.key
     }
   }
-
 }
 
 resource "aws_autoscaling_group" "asg" {
