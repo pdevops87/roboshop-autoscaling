5ac3d309b55f024e78a0fd1a189ccd58e9279a56
Your Name <vemasani.pavani@gmail.com>
2026-02-10 13:27:25 +0530

    vpc security group id

--- db_server.tf
+++ db_server.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "db_servers" {
   for_each         =  var.db_components
   ami              =  var.ami
   instance_type    = each.value["instance_type"]
-  security_groups  = [aws_security_group.sg[each.key].id]
+  vpc_security_group_ids = [aws_security_group.sg[each.key].id]
   user_data_base64 = base64encode(templatefile("${path.module}/user-data.sh", {
     component      = each.key
     env            = var.env
