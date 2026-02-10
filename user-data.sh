fa2c35da525e98a3fbd9a01dccc3ee37dabf2bcd
Your Name <vemasani.pavani@gmail.com>
2026-02-10 14:23:27 +0530

    python version changed

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 dnf install python3.13-pip -y
-pip3.11 install ansible
+pip3.13 install ansible
 ansible-pull -i localhost, -U https://github.com/pdevops87/roboshop-ansible-v4 roboshop.yaml -e component=${component} -e env=${env} >/tmp/instance.log
