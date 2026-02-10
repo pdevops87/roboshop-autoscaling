8cf121a43202e4fcc777b00a966b1c1ef2518063
Your Name <vemasani.pavani@gmail.com>
2026-02-10 18:10:36 +0530

    loop map objects

--- asg.tf
+++ asg.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "lt" {
   name = "${each.key}-${var.env}"
   image_id =var.ami
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  vpc_security_group_ids = [aws_security_group.app_sg[each.key].id]
   tag_specifications {
     resource_type = "instance"
     tags = {
