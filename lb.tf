daa1153713b3222601aec35c3999b88b299f763b
Your Name <vemasani.pavani@gmail.com>
2026-02-11 12:54:28 +0530

    check health checks

--- lb.tf
+++ lb.tf
@@ -16,6 +16,9 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "instance"
+  health_check {
+    path = "/health"
+  }
 }
 
 resource "aws_lb_listener" "listener_ports" {
